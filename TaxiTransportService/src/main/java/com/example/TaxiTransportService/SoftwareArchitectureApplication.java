843028505b63c7ee4ff79642181313edfe6cca91
thien <79550249+thienhk15@users.noreply.github.com>
2023-06-23 19:10:57 +0700

    1st commit

---
+++ TaxiTransportService/src/main/java/com/example/TaxiTransportService/SoftwareArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.TaxiTransportService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareArchitectureApplication.class, args);
+	}
+
+}
---
+++ TaxiTransportService/src/test/java/com/example/TaxiTransportService/SoftwareArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TaxiTransportService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
